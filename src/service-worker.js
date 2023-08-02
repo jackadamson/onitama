26e0f821494bcbf730b9b8e89454d680e17fc3dd
Jack Adamson <jack@mrfluffybunny.com>
2023-08-02 22:24:39 +1000

    skip waiting for service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -70,3 +70,7 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+self.addEventListener('install', () => {
+  // The promise that skipWaiting() returns can be safely ignored.
+  self.skipWaiting();
+});
